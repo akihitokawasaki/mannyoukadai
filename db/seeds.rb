669913c9cd8a01985f7d83c25d2fa3b6074c011f
akihito kawasaki <kawasaki03040304@gmail.com>
2020-05-22 19:37:15 +0900

    link追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,4 +3,11 @@
 User.create(name: "aaa", email: "aaa@gmail.com", password_digest: "aaaaaa")
 5.times do |i|
     Label.create!(name: "sample#{i + 1}")
-  end
+end
+
+User.create!(name:  "admin",
+             email: "admin@example.jp",
+             password:  "11111111",
+             password_confirmation: "11111111",
+             admin: true)
+
