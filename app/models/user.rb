46b72af9ac59a3270222f986aa4f3fc72184fc70
akihito kawasaki <kawasaki03040304@gmail.com>
2020-05-22 14:19:13 +0900

    testコード追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  before_destroy :do_not_destroy_last_admin
+  private
+  def do_not_destroy_last_admin
+    throw(:abort) if User.where(admin: true).count <= 1 && self.admin?
+  end
   has_many :tasks
   validates :name,  presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 },
@@ -6,5 +11,6 @@ class User < ApplicationRecord
                                     before_validation { email.downcase! }
                                     has_secure_password
                                     validates :password, presence: true, length: { minimum: 6 }
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :users
+  end
   resources :sessions, only: [:new, :create, :destroy]
   resources :tasks
   root 'tasks#index'
